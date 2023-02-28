18f130a5b773ab1fecb5e7ab386a509efeeb6d6f
framerage <officialigonin@mail.ru>
2023-02-28 17:28:38 +0400

    readyCube

--- src/components/appMainLayout/AppMainLayout.jsx
+++ src/components/appMainLayout/AppMainLayout.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import BulkCube from "../bulkCube/BulkCube";
 import classes from "./styles.module.css";
 const AppMainLayout = () => {
   return (
     <main className={classes.appContainer}>
-      <div className={classes.contantItem}>1</div>
+      <div className={classes.contantItem}>
+        <BulkCube />
+      </div>
       <div className={classes.contantItem}>1</div>
       <div className={classes.contantItem}>1</div>
     </main>
--- src/components/bulkCube/BulkCube.jsx
+++ src/components/bulkCube/BulkCube.jsx
@@ -1,6 +1,28 @@
 import React from "react";
 import classes from "./styles.module.css";
 const BulkCube = () => {
-  return <div></div>;
+  const figureWalls = [
+    { id: 0, name: "left", color: "red" },
+    { id: 1, name: "top", color: "blue" },
+    { id: 2, name: "bottom", color: "green" },
+    { id: 3, name: "right", color: "orange" },
+  ];
+  return (
+    <div className={classes.cubeContainer}>
+      <div className={classes.cubeShadow}></div>
+      <div className={classes.cube}>
+        {figureWalls.map((wall) => (
+          <span
+            key={wall.name}
+            className={classes.cubeWall}
+            style={{
+              ["--elem"]: `${wall.id}`,
+              backgroundColor: wall.color,
+            }}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default BulkCube;
